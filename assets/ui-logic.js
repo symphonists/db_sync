4c0c8b5d4e46c1d4b53417939daabfff3bf27143
Nick Dunn <nick@nick-dunn.co.uk>
2009-09-06 11:22:28 +0100

    More succinct JS

--- assets/ui-logic.js
+++ assets/ui-logic.js
@@ -1,7 +1,5 @@
 jQuery(document).ready(function() {
-
-	var wipeButton = jQuery(".settings button[name='action[db_sync_flush]']");
-	wipeButton.click(function() {
+	jQuery(".settings button[name='action[db_sync_flush]']").click(function() {
 		return confirm('Flusing the log will erase all log data. Are you sure?');
 	});
 });
