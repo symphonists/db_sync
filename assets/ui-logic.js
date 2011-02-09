d60625bd830df0282dcec5d514ada3ac36316331
Nick Dunn <nick.dunn@airlock.com>
2011-02-09 13:19:59 +0000

    Removed legacy files

--- assets/ui-logic.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery(".settings button[name='action[db_sync_flush]']").click(function() {
-		return confirm('Flusing the log will erase all log data. Are you sure?');
-	});
-});
